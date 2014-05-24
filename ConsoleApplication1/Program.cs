3cb2b7a48fbb550ef5ec3dc9ececbe7923cf0edf
Daniel Ranger <daniel@formatique.com>
2014-05-24 17:46:09 -0400

    Updated sample application.

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -8,6 +8,9 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             BCM2835.bcm2835_init();
+            BCM2835.bcm2835_gpio_fsel((byte)BCM2835.RPiGPIOPin.RPI_V2_GPIO_P1_11, (byte)BCM2835.bcm2835FunctionSelect.BCM2835_GPIO_FSEL_OUTP);
+            //BCM2835.bcm2835_gpio_set((byte)BCM2835.RPiGPIOPin.RPI_V2_GPIO_P1_11);
+            BCM2835.bcm2835_gpio_clr((byte)BCM2835.RPiGPIOPin.RPI_V2_GPIO_P1_11);
             BCM2835.bcm2835_close();
         }
     }
